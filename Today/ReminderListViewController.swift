77071437f92852b5c2b10249162233797952334c
Adam Khalifa <akhalifa1982@gmail.com>
2022-04-28 11:36:35 +0300

    set list layout

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+
+
+    }
+
+    func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfig = UICollectionLayoutListConfiguration(appearance: .grouped)
+
+        listConfig.backgroundColor = .clear
+        listConfig.showsSeparators = false
+
+        return UICollectionViewCompositionalLayout.list(using: listConfig)
     }
 
 
